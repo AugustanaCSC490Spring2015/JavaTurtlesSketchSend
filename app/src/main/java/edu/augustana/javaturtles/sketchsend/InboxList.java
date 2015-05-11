8e22303ebc40ec98de2c3014ace191e702fc68e7
benjaminknapper <benjaminknapper@gmail.com>
2015-05-11 15:43:26 -0500

    Not a functioning version, attempted to add Inbox functionality

--- app/src/main/java/edu/augustana/javaturtles/sketchsend/InboxList.java
+++ app/src/main/java/edu/augustana/javaturtles/sketchsend/InboxList.java
@@ -1,17 +1,54 @@
 package edu.augustana.javaturtles.sketchsend;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.widget.AbsListView.CHOICE_MODE_MULTIPLE;
 
 
 public class InboxList extends ActionBarActivity {
+
+    private static final String TAG = "Inbox";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inbox_list);
+
+        final ParseQuery<ParseObject> query = ParseQuery.getQuery("Drawings");
+        query.whereEqualTo("toUser", ParseUser.getCurrentUser().getUsername());
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> parseObjects, com.parse.ParseException e) {
+                if (e == null) {
+                   createInbox(parseObjects);
+                } else {
+                    Log.d("Inbox", "Error:");
+                }
+            }
+        });
+
+
     }
 
 
@@ -35,6 +72,31 @@ public class InboxList extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createInbox(List<ParseObject> fromQuery) {
+        List<String> fromUser = new ArrayList<String>();
+        List<String> serializedDrawing = new ArrayList<String>();
+        for (int i = 0; i < fromQuery.size(); i++) {
+            fromUser.add(fromQuery.get(i).getString("fromUser") + " " + i);
+            serializedDrawing.add(fromQuery.get(i).getString("drawingString"));
+        }
+        ListView inboxList = (ListView) findViewById(R.id.inboxList);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_item, fromUser);
+        inboxList.setAdapter(adapter);
+
+        AdapterView.OnItemClickListener inboxOnClickListener = null;
+        inboxList.setOnItemClickListener(inboxOnClickListener);
+
+        inboxOnClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent redraw = new Intent(InboxList.this, ReceivedDrawingViewer.class);
+                redraw.putExtra("stringToRedraw", serializedDrawing);
+                startActivity(redraw);
+            }
+        };
+        adapter.notifyDataSetChanged();
     }
 
     //Dialog for "Add" button
