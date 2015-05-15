c19659e8c94897ce1bc794645dd1d7de2443cfc8
Logan Kruse <logankruse11>
2015-05-14 21:42:10 -0500

    Redraw is functional, possible bug in sizing. Also a bug in create user.

--- app/src/main/java/edu/augustana/javaturtles/sketchsend/ReceivedDrawing.java
+++ app/src/main/java/edu/augustana/javaturtles/sketchsend/ReceivedDrawing.java
@@ -1,9 +1,8 @@
 package edu.augustana.javaturtles.sketchsend;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class ReceivedDrawing extends ActionBarActivity {
@@ -16,6 +15,9 @@ public class ReceivedDrawing extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_received_drawing);
 
+        ActionBar myBar = getSupportActionBar();
+        myBar.hide();
+
         theReceivedViewer = (ReceivedDrawingViewer) findViewById(R.id.the_received_viewer);
 
         Bundle bundle = getIntent().getExtras();
@@ -24,26 +26,16 @@ public class ReceivedDrawing extends ActionBarActivity {
         theReceivedViewer.setDrawingString(serializedDrawing);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_received_drawing, menu);
-        return true;
+    protected void onPause(){
+        super.onPause();
+        theReceivedViewer.setStopRecursiveCall(true);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onDestroy(){
+        super.onDestroy();
+        theReceivedViewer.setStopRecursiveCall(true);
     }
+
 }
--- app/src/main/java/edu/augustana/javaturtles/sketchsend/ReceivedDrawingViewer.java
+++ app/src/main/java/edu/augustana/javaturtles/sketchsend/ReceivedDrawingViewer.java
@@ -37,6 +37,7 @@ public class ReceivedDrawingViewer extends View {
     private TimerTask drawTask;
     private Timer timer = new Timer();
     private Handler handler = new Handler();
+    private Runnable r;
     private FullSketchObject receivedDrawing;
 
     private SingleLine currentLine;
@@ -53,8 +54,8 @@ public class ReceivedDrawingViewer extends View {
     private int screenHeight;
     private int currentLineIndex = 0;
 
-    private boolean continueLine = true;
     private boolean firstPoint = true;
+    private boolean stopRecursiveCall;
 
 
     public ReceivedDrawingViewer(Context context, AttributeSet atts) {
@@ -82,6 +83,12 @@ public class ReceivedDrawingViewer extends View {
         System.out.println(currentLine.getColor() + "   " + currentLine.getWidth() + "    " + currentLine.getSize());
     }
 
+    @Override
+    protected void onDraw(Canvas canvas){
+        // drawOnHiddenBitmap the background screen
+        canvas.drawBitmap(bitmap, 0, 0, backgroundPaint);
+    }
+
     public void setDrawingString(String serializedString) {
         this.serializedString = serializedString;
         Gson deserializer = new Gson();
@@ -92,26 +99,30 @@ public class ReceivedDrawingViewer extends View {
 
     public void startNextLine(int index) {
         currentLine = receivedDrawing.getSingleLine(index);
-        Log.w(TAG, "Initialized Line object");
         lineList = currentLine.getLine();
-        Log.w(TAG, "Initialized list:  " + lineList.size());
         currentColor = currentLine.getColor();
-        Log.w(TAG, "Initialized Color");
         currentWidth = currentLine.getWidth();
         autoPainter.setColor(currentColor);
         autoPainter.setStrokeWidth(currentWidth);
+        stopRecursiveCall=false;
         repeatAnim();
-        Log.w(TAG, "Current Values: " + currentColor + "    " + currentWidth);
+    }
+
+    public void setStopRecursiveCall(Boolean recursiveCall){
+        this.stopRecursiveCall=recursiveCall;
     }
 
     public void drawSegment() {
+        Log.w(TAG, "Draw Segment Call");
         if (firstPoint) {
             lastPoint = currentLine.getPoint();
             Log.w(TAG, lastPoint.x + "/" + lastPoint.y);
             bitmapCanvas.drawCircle(lastPoint.x, lastPoint.y, currentWidth / 2, autoPainter);
             firstPoint = false;
+            Log.w(TAG, currentLine.getSize()+"");
             invalidate();
         } else if (currentLine.getSize() > 0) {
+            Log.w(TAG, "Starting the second point");
             thisPoint = currentLine.getPoint();
             Log.w(TAG, thisPoint.x+"/"+thisPoint.y);
             bitmapCanvas.drawLine(lastPoint.x, lastPoint.y, thisPoint.x, thisPoint.y, autoPainter);
@@ -120,9 +131,15 @@ public class ReceivedDrawingViewer extends View {
             invalidate();
         } else {
             currentLineIndex++;
-            if(receivedDrawing.getSize()>=currentLineIndex){
-                //STOP THE TIMAH
+            if(receivedDrawing.getSize()<=currentLineIndex){
+                Log.w(TAG, "REMOVING CALLBACKS");
+                stopRecursiveCall=true;
+                handler.removeCallbacks(null);
+                handler.removeCallbacksAndMessages(r);
+                Log.w(TAG, "REMOVED CALLBACKS");
             }else{
+                Log.w(TAG, "CONTINUE LINE");
+                firstPoint=true;
                 startNextLine(currentLineIndex);
             }
         }
@@ -134,17 +151,22 @@ public class ReceivedDrawingViewer extends View {
 
     //Need to do more research on timers to figure out how to do animations
     public void repeatAnim() {
+
 //        drawTask = new TimerTask() {
 //            @Override
 //            public void run() {
-                handler.postDelayed(new Runnable() {
+                 r= new Runnable() {
                     @Override
                     public void run() {
-                        while (continueLine) {
+                        Log.w(TAG, "handler running");
                             drawSegment();
+                        Log.w(TAG, "return from call");
+                        if(!stopRecursiveCall) {
+                            handler.postDelayed(r, 75);
                         }
                     }
-                }, 250);
+                };
+            handler.postDelayed(r,1000);
             }
         }
 
